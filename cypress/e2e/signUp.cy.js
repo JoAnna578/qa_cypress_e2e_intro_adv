341e46481022b72c0774f992c0ddd83f534518c6
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 11:43:40 +0200

    Refactor Sign Up test to use direct form actions instead of custom command

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -2,15 +2,25 @@
 /// <reference types="../support" />
 
 import HomePageObject from '../support/pages/HomePageObject';
+import { generateUser } from '../support/generate';
 
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
     const homePage = new HomePageObject();
+    const user = generateUser();
 
-    // Używamy komendy do rejestracji nowego użytkownika
-    cy.registerNewUser().then((user) => {
-      // Sprawdzamy, czy username pojawia się w nagłówku
-      homePage.assertHeaderContainUsername(user.username);
-    });
+    // Wejdź na stronę Sign Up
+    cy.visit('/user/register');
+
+    // Wypełnij formularz
+    cy.get('[data-cy="username-sign-up"]').type(user.username);
+    cy.get('[data-cy="email-sign-up"]').type(user.email);
+    cy.get('[data-cy="password-sign-up"]').type(user.password);
+
+    // Kliknij przycisk Sign Up
+    cy.get('[data-cy="sign-up-btn"]').click();
+
+    // Sprawdź, czy username pojawia się w nagłówku
+    homePage.assertHeaderContainUsername(user.username);
   });
 });
