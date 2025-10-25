c29b942a78ff90b2eb68a77fc9ddb919a01d2b1b
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 11:38:16 +0200

    Add Sign Up test with POM and custom register command

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -1,5 +1,16 @@
+/// <reference types="cypress" />
+/// <reference types="../support" />
+
+import HomePageObject from '../support/pages/HomePageObject';
+
 describe('Sign Up page', () => {
   it('should provide an ability to register new account', () => {
+    const homePage = new HomePageObject();
 
+    // Używamy komendy do rejestracji nowego użytkownika
+    cy.registerNewUser().then((user) => {
+      // Sprawdzamy, czy username pojawia się w nagłówku
+      homePage.assertHeaderContainUsername(user.username);
+    });
   });
 });
